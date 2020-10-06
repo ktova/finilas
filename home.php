c16e7e2592ec8ae44a8315d7a8150cf9e95ae529
Tonva07 <teva@2gik.ch>
2020-10-06 13:56:11 +0200

    ss

--- home.php
+++ home.php
@@ -1,1 +1,7 @@
-Hello world
+<form id="test" name="test_pe" method="get"> <!-- Choisir la méthode POST ou GET -->
+  <div class="formdiv">
+   <label for="valeur">Entrer la valeur</label>
+   <input class="valeur" id="valeur" maxlength="32" name="valeur" required="required" type="text" placeholder="Entrez une valeur" />
+   <input class="confirm" type="submit" name="confirm" id="Chercher" style="display: block;"/>
+  </div>
+</form>
