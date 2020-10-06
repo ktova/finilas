3274760819e90b96bfad01b78edb923f3c1b0124
Tonva07 <teva@2gik.ch>
2020-10-06 16:30:08 +0200

    Update home.php

--- home.php
+++ home.php
@@ -9,36 +9,7 @@ $creds = parse_ini_file("../bin/api.ini", true);
 
 // DB Creds
 
-// Generate access token
-function generate_access_token($id, $secret){
-
-    $ch = curl_init();
-    $headers = array('Content-Type: application/x-www-form-urlencoded');
-    //$body = '{"grant_type": "client_credentials", "client_id": "'.$id.'", "client_secret": "'.$secret.'", "scope": "application_'.$id.'%20api_finilas"}';
-    $fields = [
-        'grant_type' => 'client_credentials',
-        'client_id' => $id,
-        'client_secret' => $secret,
-        'scope' => 'application_'.$id.''
-    ];
-    $body = http_build_query($fields);
-
-    curl_setopt($ch, CURLOPT_URL, 'https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=%2Fpartenaire');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-    // Return response
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-    $authToken = curl_exec($ch);
-    curl_close($ch);
-    return $authToken;
-
-}
+include('psx/gen_token.php');
 
 // Request data
 $access_token = generate_access_token($creds['id'], $creds['secret']);
