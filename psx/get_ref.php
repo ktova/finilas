39db85e9229767d1492e6af293846cff71fb42ab
Tonva07 <teva@2gik.ch>
2020-10-07 03:43:56 +0200

    Update get_ref.php

--- psx/get_ref.php
+++ psx/get_ref.php
@@ -4,18 +4,18 @@
     function generate_referentiel($access_token){
 
         $ch = curl_init();
-        $headers = array('Content-Type: application/json');
-        $fields = [
-            'Authorization' => 'Bearer '.$access_token.''
-        ];
-        $body = http_build_query($fields);
+        $headers = array('Content-Type: application/json','Athorization: Bearer '.$access_token.'');
+        //$fields = [
+        //    'Authorization' => 'Bearer '.$access_token.''
+        //];
+        //$body = http_build_query($fields);
     
-        curl_setopt($ch, CURLOPT_URL, 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/referentiel/naturesContrats');
-        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_URL, 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/referentiel/pays');
+        //curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_HEADER, 0);
     
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     
