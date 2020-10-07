c293f6dba2d133fe2a62207545ee839c93a077c5
Tonva07 <teva@2gik.ch>
2020-10-07 15:47:53 +0200

    Update home.php

--- home.php
+++ home.php
@@ -32,9 +32,7 @@ $response = $client->request('GET', 'https://api.emploi-store.fr/partenaire/offr
   ]
 ]);
 
-print_r($response->getStatusCode());
-echo '<br><br>';
-print_r($response->getReasonPhrase());
+print_r($response->getHeader());
 
 ?>
 
