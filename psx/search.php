da19fd16df8106b31702b4e861abb9ee5ede5137
Tonva07 <teva@2gik.ch>
2020-10-07 11:50:48 +0200

    Update search.php

--- psx/search.php
+++ psx/search.php
@@ -3,7 +3,7 @@
     // Get Ressources from API
     function global_search_query($access_token){
 
-        $ch = curl_init();
+        $sh = curl_init();
         $headers = array(
             'Authorization: '.$access_token.'',
             'Content-Type: application/json',
@@ -14,21 +14,21 @@
         $base_url = 'https://api.emploi-store.fr/partenaire/';
         $url = 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search?qualification=0&motsCles=informatique';
     
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($sh, CURLOPT_URL, $url);
+        curl_setopt($sh, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($sh, CURLOPT_HEADER, 0);
     
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($sh, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($sh, CURLOPT_FOLLOWLOCATION, true);
     
         // Return response
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        $search_results = curl_exec($ch);
-        print_r($ch);
+        curl_setopt($sh, CURLOPT_TIMEOUT, 30);
+        $search_results = curl_exec($sh);
+        print_r($sh);
         echo '--- <br>';
         print_r($search_results);
         echo '--- <br>';
-        curl_close($ch);
+        curl_close($sh);
         return $search_results;
     
     }
